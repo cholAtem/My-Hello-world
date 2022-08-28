2c769bd4bc3b39333d207726718cd7d623eeaf11
cholAtem <111236550+cholAtem@users.noreply.github.com>
2022-08-27 20:39:39 -0500

    Add files via upload
    Lab1 Assignment

---
+++ ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HelloWorld
+//
+//  Created by Chol Atem on 8/26/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, Chol!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
